22c866629bc7f59f8b16e226fd2e176ca48ab0a7
Nikk0o <nknk@lagartao.gay>
2025-04-10 18:33:55 -0300

    a

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ stdenv, pkgs ? import <nixpkgs> {} }:
+
+stdenv.mkDerivation {
+  pname = "lagartao-gay";
+  name = "lagartao-gay";
+  src = ./.;
+  buildInputs = with pkgs; [ jekyll ];
+
+  dontUnpack = true;
+  buildPhase = "jekyll build";
+  installPhase = ''
+    mkdir -p $out
+    cp _site/* $out'';
+}
